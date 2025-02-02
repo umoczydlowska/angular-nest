0bceabf7944f9fdcbe73024eef1404f92a247473
uchmielewska <u.chmielewska@millionmonkeyssoftware.com>
2025-02-02 15:14:01 +0100

    init basic todo app

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,45 @@
+import { NgClass, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { v4 as uuidv4 } from 'uuid';
+
+export interface TodoItem {
+  id: string;
+  task: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, NgFor, NgClass],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'frontend';
+  todoList: TodoItem[] = [];
+
+  newTask: string = '';
+
+  addTask(): void {
+    if (this.newTask.trim() !== '') {
+      const newTodoItem: TodoItem = {
+        id: uuidv4(),
+        task: this.newTask,
+        completed: false,
+      };
+
+      this.todoList.push(newTodoItem);
+      this.newTask = '';
+    }
+  }
+
+  deleteTask(taskId: string): void {
+    this.todoList = this.todoList.filter((task) => task.id !== taskId);
+  }
+
+  completeTask(index: number): void {
+    this.todoList[index].completed = !this.todoList[index].completed;
+  }
 }
